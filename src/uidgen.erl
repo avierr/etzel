fcfc26e32a0094ae85cc6792beebcbebf06031c9
AvinashD'silva <avinashdsilva@TTLx.local>
2015-08-13 14:28:29 +0530

    made an uid generator

---
+++ src/uidgen.erl
@@ -0,0 +1,64 @@
+-module(uidgen).
+
+-behaviour(gen_server).
+
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3,
+         get_random_string/2]).
+
+
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+init([]) ->
+    random:seed(os:timestamp()),
+
+    Str = uidgen:get_random_string(2,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+    Strbin = erlang:list_to_binary(Str),
+    {Mega, Secs, _} = os:timestamp(),
+
+    Timestamp = Mega*1000000 + Secs,
+    Timestampbin = erlang:integer_to_binary(Timestamp),
+
+    Prefix = iolist_to_binary([Strbin,Timestampbin,<<"_">>]),
+    io:format("\n ~w \n",[Prefix]),
+
+    {ok, {Prefix,0}}.
+
+handle_call(getuid,_From,{Prefix,Counter}) ->
+
+    Reply = iolist_to_binary([Prefix,Counter]),
+
+    {reply,Reply,{Prefix,Counter+1}};
+
+handle_call(_Request, _From, State) ->
+    {reply, ignored, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+get_random_string(Length, AllowedChars) ->
+    lists:foldl(fun(_, Acc) ->
+                        [lists:nth(random:uniform(length(AllowedChars)),
+                                   AllowedChars)]
+                            ++ Acc
+                end, [], lists:seq(1, Length)).
+%% Internal functions
